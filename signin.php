6347975c8b049c5d7b629996ac6785878d6d8694
kutkujo <115966117+kutkujo@users.noreply.github.com>
2022-10-17 10:11:29 -0500

    Update signin.php

--- signin.php
+++ signin.php
@@ -9,7 +9,7 @@ use myPHPnotes\Microsoft\Auth;
 $tenant = "common";
 $client_id = "f684b91c-dffd-4df6-8cbd-d9d524a6fabb";
 $client_secret = "HvX8Q~sQiBS7AmD9lZstljDQNRkICYZbL50Aubdc";
-$callback = "https://kutkujo.github.io/BibliotecaVirtual/callback.php";
+$callback = "https://kutkujo.github.io/BibliotecaVirtual/Callback.php";
 $scopes = ["User.Read"];
 
 $microsoft = new Auth($tenant, $client_id, $client_secret,$callback, $scopes);
